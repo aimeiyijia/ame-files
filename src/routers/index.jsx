7b6f2665fbbe0b5763a59ecf40ea88d4a01a2b31
aimeiyijia <1456723200@qq.com>
2020-08-16 00:09:14 +0800

    调整引入模块

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Redirect } from "react-router";
 import Layout from "@/layout/index.jsx";
-import Dashboard from "@/views/dashboard/Dashboard";
+import Dashboard from "@/views/dashboard/dashboard";
 
 const routes = [
 	{
