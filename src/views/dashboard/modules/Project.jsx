7d58390de5385b6cbe2ad606d2c3241c0a2591a9
aimeiyijia <1456723200@qq.com>
2020-08-17 07:36:30 +0800

    增加dashboard中项目预览

--- src/views/dashboard/modules/Project.jsx
+++ src/views/dashboard/modules/Project.jsx
@@ -1,9 +1,19 @@
 import React from "react";
-class Project extends React.Component{
-    render(){
-        return (
-            <div className="ame-project">工程区</div>
-        )
-    }
+import { Input } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+
+class Project extends React.Component {
+	render() {
+		return (
+			<div className="ame-project">
+				<div className="search">
+					<Input size="large" placeholder="large size" prefix={<SearchOutlined />} />
+				</div>
+				<div className="statistics"></div>
+				<div className="folders"></div>
+				<div className="recent"></div>
+			</div>
+		);
+	}
 }
 export default Project;
