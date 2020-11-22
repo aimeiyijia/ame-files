c89dd3c08ed962eeb773fe5418db0ddb1bda7731
aimeiyijia <1456723200@qq.com>
2020-11-22 09:13:39 +0800

    格式化代码

--- src/views/files/index.jsx
+++ src/views/files/index.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import { Row, Col } from "antd";
-import Directory from "./modules/Directory";
-import Path from "./modules/Path";
-import ViewFile from "./modules/ViewFile";
-import "./index.scss";
+import React from 'react'
+import { Row, Col } from 'antd'
+import Directory from './modules/Directory'
+import Path from './modules/Path'
+import ViewFile from './modules/ViewFile'
+import './index.scss'
 
 export default class Files extends React.Component {
-	render() {
-		return (
-			<div className="ame-dashboard">
-				<Row gutter={16} className="dashboard-row">
-					<Col xs={12} sm={12} md={10} lg={8} xl={6}>
-						<Directory />
-					</Col>
-					<Col xs={12} sm={12} md={14} lg={16} xl={18}>
-						<Path />
-						<ViewFile />
-					</Col>
-				</Row>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="ame-dashboard">
+        <Row gutter={16} className="dashboard-row">
+          <Col xs={12} sm={12} md={10} lg={8} xl={6}>
+            <Directory />
+          </Col>
+          <Col xs={12} sm={12} md={14} lg={16} xl={18}>
+            <Path />
+            <ViewFile />
+          </Col>
+        </Row>
+      </div>
+    )
+  }
 }
--- src/views/files/modules/ViewFile.jsx
+++ src/views/files/modules/ViewFile.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import AmeScrollbars from "@/compontents/AmeScrollbars.jsx";
-import video from "@/assets/testFiles/video.mp4";
+import React from 'react'
+import AmeScrollbars from '@/compontents/AmeScrollbars.jsx'
+import video from '@/assets/testFiles/video.mp4'
 
 // 左侧-目录区
 class ViewFile extends React.Component {
-	constructor(props) {
-		super(props);
-		console.log(video);
-	}
-	render() {
-		return (
-			<div className="ame-file-view">
-				<video src={video}></video>
-			</div>
-		);
-	}
+  constructor(props) {
+    super(props)
+    console.log(video)
+  }
+  render() {
+    return (
+      <div className="ame-file-view">
+        <video src={video} controls="controls"></video>
+      </div>
+    )
+  }
 }
-export default ViewFile;
+export default ViewFile
